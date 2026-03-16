18230a71ea9f403781d6febfdd70ac54160548a3
Zakk <zakariazakkon@gmail.com>
2026-03-16 17:48:14 +0300

    fix turbopack build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const withPWA = require("next-pwa")({
-  dest: "public"
+  dest: "public",
+  disable: process.env.NODE_ENV === "development"
 })
 
-module.exports = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true
-})
+}
+
+module.exports = withPWA(nextConfig)
