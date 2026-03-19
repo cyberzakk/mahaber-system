791619a522a63880ddf91c8e3a0fcee0ac5e32be
Zakk <zakariazakkon@gmail.com>
2026-03-19 09:15:31 +0300

    Payment  visible

--- src/app/payments/page.js
+++ src/app/payments/page.js
@@ -46,9 +46,9 @@ export default function PaymentsPage() {
   return (
     <div className="p-10">
 
-      <h1 className="text-3xl font-bold mb-6">
-        Payments
-      </h1>
+      <h1 className="text-3xl font-bold mb-6 text-gray-900">
+  Payments
+</h1>
 
       <div className="overflow-x-auto bg-white rounded-lg shadow border border-gray-300">
 
